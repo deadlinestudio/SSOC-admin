8a681740c0657e97a20b207623162c1aa77ac888
os.kwon <93negaro@gmail.com>
2020-12-11 13:05:28 +0900

    log check에 alert modal 추가

--- src/views/log/Log.js
+++ src/views/log/Log.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   CButton,
@@ -11,6 +11,7 @@ import {
 } from "@coreui/react";
 
 import { getLog, initLog } from "../../modules/log/log";
+import { AlertModal } from "../notification/modals/Modals";
 
 const Log = () => {
   const dispatch = useDispatch();
@@ -19,12 +20,20 @@ const Log = () => {
     initDone: log.initDone,
     getDone: log.getDone,
   }));
+  const [logModal, setLogModal] = useState(false);
 
   // 컴포넌트가 처음 렌더링될 때 초기화
   useEffect(() => {
-    console.log("로그 초기화");
+    console.log("로그 초기화 시작");
     dispatch(initLog());
   }, [dispatch]);
+
+  // 초기화 후 렌더링
+  useEffect(() => {
+    if (initDone !== true) return;
+
+    console.log("로그 초기화 끝");
+  }, [initDone]);
 
   // 로그 가져온 후 렌더링
   useEffect(() => {
@@ -33,21 +42,31 @@ const Log = () => {
     console.log("get log success");
     console.log("getDone : ", getDone);
     dispatch(initLog());
-  },[getDone,dispatch]);
+  }, [getDone, dispatch]);
 
   // 폼 등록 이벤트 핸들러
-  const onSubmit = (e) => {
-    e.preventDefault();
-
+  const onSubmit = () => {
     const value = document.getElementById("date-input").value;
-    if (value === "") console.log("날짜를 선택하세요");
-    else{
-      const year = value.substring(0,4);
-      const month = value.substring(5,7);
-      const date = value.substring(8,10);
-      console.log("로그 api 호출", year,month,date);
-      dispatch(getLog({year, month, date}));
-    } 
+    if (value === "") openModal();
+    else {
+      const year = value.substring(0, 4);
+      const month = value.substring(5, 7);
+      const date = value.substring(8, 10);
+      console.log("로그 api 호출", year, month, date);
+      dispatch(getLog({ year, month, date }));
+    }
+  };
+
+  // 경고 모달 열기
+  const openModal = () => {
+    console.log("경고 모달 열기");
+    setLogModal(true);
+  };
+
+  // 경고 모달 종료
+  const closeModal = () => {
+    console.log("경고 모달 닫기");
+    setLogModal(false);
   };
 
   return (
@@ -78,12 +97,18 @@ const Log = () => {
                   name="textarea-input"
                   id="textarea-input"
                   rows="15"
-                  value = {logContent}
+                  value={logContent}
                 />
               </CCol>
             </CRow>
           </CCardBody>
         </CCard>
+        <AlertModal
+          visible={logModal}
+          title={"경고"}
+          body={"날짜를 선택하여 주세요."}
+          onCancel={closeModal}
+        />
       </CCol>
     </CRow>
   );
